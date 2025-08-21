892dc41bf6e0d8b5e5df5953b4f128267755cd4e
suchetrana <suchetrana27@gmail.com>
2025-08-21 22:04:19 +0530

    updating dummyfiledownloadingCallback.js

--- Promises/IOC/dummyfiledownloadingCallback.js
+++ Promises/IOC/dummyfiledownloadingCallback.js
@@ -5,6 +5,8 @@ function download(url, cb){
         console.log("download complete in 5s");
     }, 5000)
     cb(data);
+    cb(data);  // IOC condition you want cb() only for 1 time
+                // download fn have not control on this fn
 }
 download("suchet.com", function exe(data){
     console.log("downloaded data is: ", data);
